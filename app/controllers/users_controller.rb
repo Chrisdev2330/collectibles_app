8de981f2b2a88fa830ce213a1697b9e9098db317
christian <christianmorenodumit@gmail.com>
2022-04-13 16:03:01 -0400

    Validaciones

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,27 @@
 class UsersController < ApplicationController
+    def new
+        @user= User.new
+    end
     
+    def create
+        @user = User.new user_params
+
+        if @user.save
+            redirect_to new_user_path, notice: "Usuario creado correctamente"
+        else
+            
+            render :new, status: :unprocessable_entity # muestra el status
+    
+        end
+        
+        
+    end
+    
+
+
+    private
+
+    def user_params
+        params.require(:user).permit(:name, :email, :password)
+    end
 end
